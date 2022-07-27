0e59743859fe26e4253df9eb3cf46e95d7c9c1d0
Sam Freeman <samcfreeman888@gmail.com>
2022-07-27 17:31:19 -0500

    stuck on #6.  Issue with undefined

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -48,6 +48,10 @@ console.log(isPositive(-4));
 
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
+
+//I was confused if we are supposed to add something here since it was already written below!
+//The function works though!
+
 console.log( 'isPositive - should say true', isPositive(3) );
 console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
@@ -55,10 +59,19 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
 
+function getLast( array ) {
+  if(array.length >= 1){
+    let lastElement = array[array.length -1];
+    return lastElement
+} 
+else {
+  return undefined;
+}
 }
 
+console.log(getLast(['bob', 'sue', 'tom']));
+console.log(getLast());
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
